0b8a63458bed37a7975ba7337ddfbcf6a1c39896
webPlane8 <webplane8@gmail.com>
2024-05-27 16:25:16 +0500

    In this class I learn how to install boostrtrap using cmd and how bootstrap component work with react

---
+++ 2-bootstrap/learning bootstrap/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <button type="button" class="btn btn-outline-primary">
+        Primary
+      </button>
+      <button type="button" class="btn btn-outline-secondary">
+        Secondary
+      </button>
+      <button type="button" class="btn btn-outline-success">
+        Success
+      </button>
+      <button type="button" class="btn btn-outline-danger">
+        Danger
+      </button>
+      <button type="button" class="btn btn-outline-warning">
+        Warning
+      </button>
+      <button type="button" class="btn btn-outline-info">
+        Info
+      </button>
+      <button type="button" class="btn btn-outline-light">
+        Light
+      </button>
+      <button type="button" class="btn btn-outline-dark">
+        Dark
+      </button>
+    </div>
+  );
+}
+
+export default App;
